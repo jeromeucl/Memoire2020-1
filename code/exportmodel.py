08d7724b4b4ab2066062438b12fd51f32d4469d0
deeroen <cocoloulouj@hotmail.com>
2019-10-21 14:02:10 +0200

    02/10/2019 mémoire guit creation2

--- code/exportmodel.py
+++ code/exportmodel.py
@@ -7,31 +7,31 @@ from sklearn.model_selection import train_test_split
 from Machine_learning import worktbl,tbl,matching
 import pickle
 
-matching.remove('4011_frequency')
-matching = [x for x in matching if not x.startswith('3')]
-model_param = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\metaparam\FINALTBL2019-10-14.csv")
-for exo in matching:
+#matching.remove('4011_frequency')
+#matching = [x for x in matching if not x.startswith('3')]
+#model_param = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\metaparam\FINALTBL2019-10-14.csv")
+#for exo in matching:
+#
+#    label = tbl[exo].notnull().astype(int).to_frame()
+#    # Split the data and the label into test and train set
+#    train, test, label_train, label_test = train_test_split(worktbl, label, test_size=0.2)
+#    # If this exercise was never used by the physio, don't run the algorithm
+#    if sum(label_train.values) != 0:
+#        # Train prediction
+#        param = model_param[model_param['exercise_number'] == exo]
+#        clf = tree.DecisionTreeClassifier(max_depth=param['max_depth'].values[0],criterion=param['criterion'].values[0],min_samples_split=param['min_samples_split'].values[0],min_impurity_decrease=param['min_impurity_decrease'].values[0])
+#        clf = clf.fit(train, label_train)
+#        # Predict the label for train set
+#        pickle.dump(clf, open("modeltoexport\\modelfor_"+str(exo)+".sav",'wb'))
+#    else:
+#        print('Issue with label_train.values')
 
-    label = tbl[exo].notnull().astype(int).to_frame()
-    # Split the data and the label into test and train set
-    train, test, label_train, label_test = train_test_split(worktbl, label, test_size=0.2)
-    # If this exercise was never used by the physio, don't run the algorithm
-    if sum(label_train.values) != 0:
-        # Train prediction
-        param = model_param[model_param['exercise_number'] == exo]
-        clf = tree.DecisionTreeClassifier(max_depth=param['max_depth'].values[0],criterion=param['criterion'].values[0],min_samples_split=param['min_samples_split'].values[0],min_impurity_decrease=param['min_impurity_decrease'].values[0])
-        clf = clf.fit(train, label_train)
-        # Predict the label for train set
-        pickle.dump(clf, open("modeltoexport\\modelfor_"+str(exo)+".sav",'wb'))
-    else:
-        print('Issue with label_train.values')
-
-clf2 = pickle.load(open("modeltoexport\\modelfor_1006_frequency.sav", 'rb'))
-test_pred = clf.predict(test)
-
-balanced_accuracy_score(label_test, test_pred)
+clf2 = pickle.load(open("modeltoexport\\modelfor_1001_frequency.sav", 'rb'))
+#test_pred = clf2.predict(test)
 
 
 
+
+tree.plot_tree(clf2)
 
 
--- code/exs_effect.py
+++ code/exs_effect.py
@@ -1,5 +1,5 @@
-#'PaIn2' pain during the day
-#'PaIn1 pain during the night
+# 'PaIn2' pain during the day
+# 'PaIn1 pain during the night
 import sys
 
 import matplotlib.pyplot as plt
@@ -11,6 +11,7 @@ from Machine_learning import patient_daily_data
 from Machine_learning import exercise_scheme
 from Machine_learning import Working_Directory
 from datetime import date
+
 cols = exercise_scheme.columns.drop(
     list(exercise_scheme.filter(regex='intensity')) + list(
         exercise_scheme.filter(regex='actual')))
@@ -20,31 +21,32 @@ exercise_scheme = exercise_scheme.apply(
 
 Variabletochec = 'PaIn2'
 Pain_threshold = 20
-pdt0 = patient_daily_data[['patient_id','day',Variabletochec]]
+pdt0 = patient_daily_data[['patient_id', 'day', Variabletochec]]
 pdt0 = pdt0.fillna(method='bfill')
 pdt0 = pdt0.fillna(method='ffill')
 
-def two_var_cor(pd_dy_var_tbl,pain_threshold,tbl_exsch,variabletochec,number_of_different_day):
+
+def two_var_cor(pd_dy_var_tbl, pain_threshold, tbl_exsch, variabletochec, number_of_different_day):
     cp = pd_dy_var_tbl.copy()
     cp['day'] -= number_of_different_day
     cp.rename(columns={variabletochec: str(number_of_different_day)}, inplace=True)
     table = pd.merge(pd_dy_var_tbl, cp, on=['patient_id', 'day'], how='inner')
     table = pd.merge(tbl_exsch, table, on=['patient_id', 'day'], how='inner')
-    table[str('diff'+str(number_of_different_day))] = np.where(table[variabletochec] - table[str(number_of_different_day)] < pain_threshold, 0, 1)
+    table[str('diff' + str(number_of_different_day))] = np.where(
+        table[variabletochec] - table[str(number_of_different_day)] < pain_threshold, 0, 1)
     return table
 
-worktl = two_var_cor(pdt0,Pain_threshold,exercise_scheme,Variabletochec,1)
+
+worktl = two_var_cor(pdt0, Pain_threshold, exercise_scheme, Variabletochec, 1)
 corrtbl = worktl.corr()
-res = corrtbl.iloc[:,-1]
+res = corrtbl.iloc[:, -1]
 res.drop(res.tail(3).index, inplace=True)
-for i in range(2,30):
-
-
+for i in range(2, 30):
     worktl = two_var_cor(pdt0, Pain_threshold, exercise_scheme, Variabletochec, i)
     corrtbl = worktl.corr()
-    TR = corrtbl.iloc[:,-1]
+    TR = corrtbl.iloc[:, -1]
     TR.drop(TR.tail(3).index, inplace=True)  # drop last n rows
     res = pd.concat([res, TR], axis=1)
 
 res.max().to_frame()
-res.to_csv(Working_Directory+"\Results_pearson_exo_increasepain"+Variabletochec+str(date.today())+".csv")
+res.to_csv(Working_Directory + "\Results_pearson_exo_increasepain" + Variabletochec + str(date.today()) + ".csv")
