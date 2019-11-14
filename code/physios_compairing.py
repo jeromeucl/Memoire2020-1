16ac75c5f8b74537373d5a2d58a9c9a8e03a484f
deeroen <cocoloulouj@hotmail.com>
2019-11-14 15:20:33 +0100

    update 14/11 no 4

--- code/physios_compairing.py
+++ code/physios_compairing.py
@@ -1,26 +1,50 @@
 '''This file create a table with the predictoin for each exercise and the exercise proposed by the physios'''
 from Machine_learning import *
 import pickle
-#load protocol and merge it with exercise shceme
+
+# load protocol and merge it with exercise shceme
 protocoltbl_hip = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\comparativetbl\protocol\protocol_hip.csv")
 protocoltbl_hip.rename(columns={"Days": "day"}, inplace=True)
 protocoltbl_knee = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\comparativetbl\protocol\protocol_knee.csv")
 protocoltbl_knee.rename(columns={"Days": "day"}, inplace=True)
-prot_pt_tbl_hip = pd.merge(exercise_scheme, protocoltbl_hip, on=['day'], how='right')
-prot_pt_tbl_knee = pd.merge(exercise_scheme, protocoltbl_knee, on=['day'], how='right')
+prot_pt_tbl_hip = pd.merge(exercise_scheme, protocoltbl_hip, on=['day'], how='left')
+prot_pt_tbl_knee = pd.merge(exercise_scheme, protocoltbl_knee, on=['day'], how='left')
+
+knee_patient = tbl[tbl['limb'].str.contains("Hip")][['patient_id']]
+hip_patient = tbl[tbl['limb'].str.contains("Knee")][['patient_id']]
+
+knee_worktbl = worktbl[worktbl['patient_id'].isin(list(knee_patient['patient_id'].unique()))]
+hip_worktbl = worktbl[worktbl['patient_id'].isin(list(hip_patient['patient_id'].unique()))]
+
+final_knee_worktbl = pd.merge(knee_worktbl, prot_pt_tbl_knee, on=['patient_id', 'day'], how='left')
+final_hip_worktbl = pd.merge(hip_worktbl, prot_pt_tbl_hip, on=['patient_id', 'day'], how='left')
 
 matching.remove('4011_frequency')
 matching = [x for x in matching if not x.startswith('3')]
 
 
-def compare_protocol_PT(prot_pt_tbl,hip_or_knee):
-    Returntbl = prot_pt_tbl[['patient_id','day']].copy()
+def compare_protocol_PT(prot_pt_tbl, ex_number):
+    Returntbl = prot_pt_tbl[['patient_id', 'patientnumber']].copy()
     Returntbl['day'] = prot_pt_tbl['day']
+    Returntbl['PT_decision'] = prot_pt_tbl[ex_number + "_frequency"].notnull().astype(int).to_frame()[
+        ex_number + "_frequency"]
+    Returntbl['Protocol_advice'] = prot_pt_tbl[ex_number]
+    clf2 = pickle.load(
+        open("C:\\Users\cocol\PycharmProjects\Memoire2020\code\modeltoexport\modelfor_" + str(ex_number) + "_frequency.sav",
+             'rb'))
+    workingtbl = prot_pt_tbl.drop(['patientnumber', 'date', 'surgery_date', 'patient_id'], axis=1)
+    Returntbl['model_prediction'] = list(clf2.predict(workingtbl))
 
-    exexcise_list = [s for s in prot_pt_tbl.columns if s.isdigit()]
-    for ex_number in exexcise_list:
-        Returntbl['PT_Protocol_difference_'+hip_or_knee+'_' + ex_number] = (prot_pt_tbl[ex_number + "_frequency"].notnull().astype(int).to_frame()[ex_number + "_frequency"] - prot_pt_tbl[ex_number]).abs()
+    Returntbl['PT_Protocol_difference_' + ex_number] = (
+                prot_pt_tbl[ex_number + "_frequency"].notnull().astype(int).to_frame()[ex_number + "_frequency"] -
+                prot_pt_tbl[ex_number]).abs()
+    Returntbl['PT_model_difference_' + ex_number] = (
+            prot_pt_tbl[ex_number + "_frequency"].notnull().astype(int).to_frame()[ex_number + "_frequency"] -
+            Returntbl['model_prediction']).abs()
+    Returntbl['Protocol_model_difference_' + ex_number] = (Returntbl['Protocol_advice'] -
+                                                           Returntbl['model_prediction']).abs()
 
     return Returntbl
-for exo in matching:
-    clf2 = pickle.load(open("modeltoexport\\modelfor_"+str(exo)+".sav", 'rb'))
+
+
+table = compare_protocol_PT(final_knee_worktbl, '1001')
