d050616f713945a13a35076bcdc1d0145ee591dc
deeroen <cocoloulouj@hotmail.com>
2019-11-28 14:00:09 +0100

    update 28/11

--- code/feature_select.py
+++ code/feature_select.py
@@ -1,30 +1,61 @@
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+from sklearn import feature_selection, tree
 import pandas as pd
 from Machine_learning import tbl, worktbl, matching
 from sklearn import preprocessing
 
-def features_selection(features_df, target,nb_of_features):
+import numpy as np
+
+
+def features_selection(features_df, target, nb_of_features):
+    scaled_worktbl = features_df.drop(['patientnumber', 'date', 'surgery_date', 'patient_id'], axis=1)
     # normalize the table to
-
-    #https://stackoverflow.com/questions/39839112/the-easiest-way-for-getting-feature-names-after-running-selectkbest-in-scikit-le
-    # Create and fit selector
-    selector = SelectKBest(chi2, k=nb_of_features)
-    selector.fit_transform(features_df, target)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    a_scaled = pd.DataFrame(min_max_scaler.fit_transform(scaled_worktbl))
+    a_scaled.columns = scaled_worktbl.columns
+    # https://stackoverflow.com/questions/39839112/the-easiest-way-for-getting-feature-names-after-running-selectkbest-in-scikit-le
+    # Create and fit selector with MI
+    selector = SelectKBest(feature_selection.mutual_info_classif, k=nb_of_features)
+    selector.fit_transform(a_scaled, target.notnull().astype(int).to_frame())
     # Get columns to keep
-    cols = selector.get_support(indices=True)
-    # Create new dataframe with only desired columns, or overwrite existing
-    df = features_df.iloc[cols]
+    cols = a_scaled.columns[selector.get_support(indices=True)]
 
-    return df
-#features_selection(worktbl,label,6)
+    # Select variables with tree
+    clf = tree.DecisionTreeClassifier(max_depth=5, class_weight='balanced')
+    clf = clf.fit(scaled_worktbl, target.fillna(method='bfill').notnull().astype(int).to_frame())
+    # Get the most important feature
+    importances = clf.feature_importances_
+
+    best_feature = list(scaled_worktbl.columns[np.flip(np.argsort(importances)[-nb_of_features:])])
+
+    features = list(set(list(cols)) | set(list(best_feature)))
+    # Create new dataframe with only desired columns, or overwrite existing
+    a_scaled = a_scaled[features]
+
+    return a_scaled
+
+a = features_selection(worktbl, tbl['1001_frequency'], 20)
+
+# features_selection(worktbl,label,6)
 scaled_worktbl = worktbl.drop(['patientnumber', 'date', 'surgery_date', 'patient_id'], axis=1)
-a = pd.DataFrame(data = {'col1': [1000, 200,300], 'col2': [10, 4,1],'col3': [1000, 200,300]})
+
+nb_of_best_features = 20
 
 min_max_scaler = preprocessing.MinMaxScaler()
 a_scaled = pd.DataFrame(min_max_scaler.fit_transform(scaled_worktbl))
 a_scaled.columns = scaled_worktbl.columns
-selector = SelectKBest(chi2, k=3)
-selector.fit_transform(a_scaled, tbl['1001_frequency'].fillna(method='bfill').fillna(method='ffill'))
+selector = SelectKBest(feature_selection.mutual_info_classif, k=nb_of_best_features)
+selector.fit_transform(a_scaled, tbl['1001_frequency'].notnull().astype(int).to_frame())
 cols = selector.get_support(indices=True)
-a_scaled = a_scaled.iloc[cols]
+colname = a_scaled.columns[cols]
+
+
+
+clf = tree.DecisionTreeClassifier(max_depth=5, class_weight='balanced')
+clf = clf.fit(scaled_worktbl, tbl['1001_frequency'].notnull().astype(int).to_frame())
+# Get the most important feature
+importances = clf.feature_importances_
+
+best_feature = scaled_worktbl.columns[np.flip(np.argsort(importances)[-nb_of_best_features:])]
+
--- code/physios_compairing.py
+++ code/physios_compairing.py
@@ -41,16 +41,32 @@ def compare_protocol_PT(prot_pt_tbl, ex_number,predtbl):
 
     return Returntbl
 #knee
-matchi = [x for x in matching if x.startswith('1')]
-for i in matchi:
+matchi_knee = [x for x in matching if x.startswith('1')]
+matchi_hip = [x for x in matching if x.startswith('2')]
+matchis = matchi_hip + matchi_knee
+#for i in matchis:
+#
+#    table = compare_protocol_PT(final_knee_worktbl, i.replace('_frequency',''),knee_worktbl)
+#    table.to_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\comparativetbl\comparetbl_"+i+str(date.today())+".csv")
 
-    table = compare_protocol_PT(final_knee_worktbl, i.replace('_frequency',''),knee_worktbl)
-    table.to_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\comparativetbl\comparetbl_"+i+str(date.today())+".csv")
+def proposition_tbl(prot_pt_tbl, matchings,predtbl):
+    Returntbl = prot_pt_tbl[['patient_id', 'patientnumber']].copy()
+    Returntbl['day'] = prot_pt_tbl['day']
+    for ex_number in matchings:
+        clf2 = pickle.load(open("C:\\Users\cocol\PycharmProjects\Memoire2020\code\modeltoexport\modelfor_" + str(ex_number) + ".sav",'rb'))
+        workingtbl = predtbl.drop(['patientnumber', 'date', 'surgery_date', 'patient_id'], axis=1).copy()
+
+        Returntbl[str(ex_number).replace('_frequency','')+'_model'] = list(clf2.predict(workingtbl))
 
 
-#hip
-matchi = [x for x in matching if x.startswith('2')]
-for i in matchi:
 
-    table = compare_protocol_PT(final_hip_worktbl, i.replace('_frequency',''),hip_worktbl)
-    table.to_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\comparativetbl\comparetbl_"+i+str(date.today())+".csv")
+    return Returntbl
+
+tablehip = proposition_tbl(final_hip_worktbl, matchi_hip,hip_worktbl)
+spike_cols = [col for col in tablehip.columns if 'model' in col]
+tablehip['number_of_proposed_ex'] = tablehip[spike_cols].sum(axis=1)
+tablehip.to_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\exercise_proposition\Hip_exos_proposition_"+str(date.today())+".csv")
+tableknee = proposition_tbl(final_knee_worktbl, matchi_knee,knee_worktbl)
+spike_cols = [col for col in tableknee.columns if 'model' in col]
+tableknee['number_of_proposed_ex'] = tableknee[spike_cols].sum(axis=1)
+tableknee.to_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\exercise_proposition\Knee_exos_proposition_"+str(date.today())+".csv")
