62788bf634485481ca1a581e812a9e20c3cacab8
deeroen <cocoloulouj@hotmail.com>
2019-12-16 00:13:37 +0100

    16/12

--- code/other_models.py
+++ code/other_models.py
@@ -6,17 +6,22 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 from feature_select import features_selection
 from sklearn.metrics import balanced_accuracy_score, make_scorer
+from sklearn.linear_model import LogisticRegression
 BCR = make_scorer(balanced_accuracy_score)
 
 
 tbl_db = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\online_tbl2019-12-09.csv")
 worktbl_db = pd.read_csv("C:\\Users\cocol\Desktop\memoire\Jéjé_work\online_worktbl2019-12-09.csv")
+matching = [s for s in tbl_db if "frequency" in s]
+matching.remove("9999_frequency")
+matching = [x for x in matching if not x.startswith('3')]
+matching = [x for x in matching if not x.startswith('4')]
 
-scaled = features_selection(worktbl_db, tbl_db['1001_frequency'], 10)
-X_train, X_test, y_train, y_test = train_test_split(scaled, tbl_db['1001_frequency'].notnull().astype(int).to_frame(), test_size=0.2, random_state=42)
 
 
-k_range = list(range(1,20)) + [30,50,100]
+'''X_train, X_test, y_train, y_test = train_test_split(scaled, tbl_db['1001_frequency'].notnull().astype(int).to_frame(), test_size=0.2, random_state=42)
+
+k_range = list([5,10,15,20,30,50,100,200])
 weight_options = ["uniform", "distance"]
 
 param_grid = dict(n_neighbors = k_range, weights = weight_options)
@@ -38,7 +43,7 @@ clf = grid.best_estimator_
 clf.fit(X_train, y_train)
 
 pred = clf.predict(X_test)
-print(balanced_accuracy_score(y_test, pred))
+print(balanced_accuracy_score(y_test, pred))'''
 
 def modeltest(model,scaleds,label,parameters):
     x_train, x_test, Y_train, Y_test = train_test_split(scaleds,
@@ -57,10 +62,20 @@ def modeltest(model,scaleds,label,parameters):
     clfs.fit(x_train, Y_train)
 
     predi = clfs.predict(x_test)
-    bcr=balanced_accuracy_score(Y_test, predi)
-    print(bcr)
+    bcr_test=balanced_accuracy_score(Y_test, predi)
 
-    return bcr
 
-from sklearn.linear_model import LogisticRegression
-result = modeltest(LogisticRegression(),scaled,tbl_db['1001_frequency'],dict(random_state=[0]))
+    return bcr_test,grids.best_estimator_
+
+k_range = list([5,10,15,20,30,50,100,200])
+weight_options = ["uniform", "distance"]
+
+param_grid = dict(n_neighbors = k_range, weights = weight_options)
+results = pd.DataFrame(columns=["Exercise", "Type_of_algorithm", "Bcr_test"])
+for i in matching[5:44]:
+    print(i)
+    scaled = features_selection(worktbl_db, tbl_db[i], 10)
+    result,best_algo = modeltest(KNeighborsClassifier(), scaled, tbl_db[i], param_grid)
+    print(result)
+    results = results.append({"Exercise": i, "Type_of_algorithm": best_algo,  "Bcr_test": result},
+        ignore_index=True)
